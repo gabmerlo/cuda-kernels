ad967b00901b4e4164539f8d689ef3f56dee837d
Gabriel Merlo <151958674+codermtk@users.noreply.github.com>
2026-08-25 14:19:00 +0200

    Tensor Cores First Functional Code I managed to finish my tensor core code, very basic implementation, just to get used to how the commands work. I wasn't expecting it to go this slow tho: 342.3 GFLOP/s (because I am using tensor cores, but I also haven't optimized this code at all) The results seems to be correct, although I seem to have a bug that is causing my max abs diff to spike ar certain positions, while leaving the rest correct. I will have to fix that next, to assure full correctness of my result as the first step before optimizing this much further.

--- tensor-cores/tensor_cores.cu
+++ tensor-cores/tensor_cores.cu
@@ -25,12 +25,53 @@ __global__ void tensor_cores(int A_num_fil, int A_num_col,float *A, int B_num_fi
     wmma::fragment<wmma::matrix_b,tensor_M,tensor_N,tensor_K,half, wmma::row_major> b_fragment;
     wmma::fragment<wmma::accumulator,tensor_M,tensor_N,tensor_K,float> accumulator_frag;
 
-    wmma::fill_fragment(accumulator, 0.0f);
+    wmma::fill_fragment(accumulator_frag, 0.0f);
 
     //Índices
-    int warp_index = (blockDim.x * blockIdx.y + threadIdx.x)/warpSize;
+    int global_warp_index = (blockDim.x * blockIdx.y + threadIdx.x)/warpSize;
+    int global_column = blockDim.x * blockIdx.x;
+    int global_row = blockDim.y * blockIdx.y;
 
+    //Shared memory
+    __shared__ half shared_memory_1[tensor_M][tensor_K];
+    __shared__ half shared_memory_2[tensor_K][tensor_N];
+
+    //Looping through tiles
+    for (int i = 0; i < A_num_col/tensor_K; i ++){
+
+        int A_tile_col = i * tensor_K;
+        int A_tile_row = global_row;
+        int B_tile_col = global_column;
+        int B_tile_row = i * tensor_K;
+
+        int A_pointer = A_tile_row * A_num_col + A_tile_col;
+        int B_pointer = B_tile_row * B_num_col + B_tile_col;
+
+
+
+        for(int i = 0; i < 8; i ++){
+            int thread_A_pointer = A_pointer + threadIdx.x%16 + (threadIdx.y%2)*A_num_col + i*A_num_col*2;
+            int thread_B_pointer = B_pointer + threadIdx.x%16 + (threadIdx.y%2)*B_num_col + i*B_num_col*2;
+
+            shared_memory_1[threadIdx.y%2 + i*2][threadIdx.x] = A[thread_A_pointer];
+            shared_memory_2[threadIdx.y%2 + i*2][threadIdx.x] = B[thread_B_pointer];
+        }
+
+        __syncthreads();
+
+        wmma::load_matrix_sync(a_fragment, &shared_memory_1[0][0], tensor_K);
+        wmma::load_matrix_sync(b_fragment, &shared_memory_2[0][0], tensor_N);
+
+        wmma::mma_sync(accumulator_frag, a_fragment, b_fragment, accumulator_frag);
+
+    }
+
+    __syncthreads();
     
+    int puntero_resultado_C = global_row*B_num_col + global_column;
+
+    wmma::store_matrix_sync(&C[puntero_resultado_C], accumulator_frag, B_num_col, wmma::mem_row_major);
+
 }
 
 int main(){
@@ -48,14 +89,15 @@ int main(){
     float *h_A = (float*)malloc(bytes_AB);
     float *h_B = (float*)malloc(bytes_AB);
     float *h_C = (float*)malloc(bytes_C);
+    float *h_C_2 = (float*)malloc(bytes_C);
 
     for (int i = 0; i < N_A_B; i ++){
         h_A[i] = dist(gen);
         h_B[i] = dist(gen);
     }
-    
+
     auto t0 = std::chrono::high_resolution_clock::now();
-    
+
     for (int i = 0; i < A_num_fil; i++) {
         for (int j = 0; j < B_num_col; j++) {
             float sum = 0.0f;
@@ -65,16 +107,17 @@ int main(){
         }
     }
     for(int i = 0; i < 5; i ++){
-        printf("%f\n",h_C[i]);
+        printf("%f\n",h_C[N_C-i]);
     }
 
     auto t1 = std::chrono::high_resolution_clock::now();
     double cpu_ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
     printf("CPU: %.1f ms\n", cpu_ms);
-    
+
     float *d_A;
     float *d_B;
     float *d_C;
+    float *d_C_2;
 
     cudaMalloc(&d_A, bytes_AB);
     cudaMalloc(&d_B, bytes_AB);
@@ -94,12 +137,12 @@ int main(){
     cudaEventCreate(&stop);
 
     for (int i = 0; i < 3; i++)
-        tiled_matmul_2d<<<blocks,threads>>>(A_num_fil, A_num_col, d_A, B_num_fil, B_num_col, d_B, d_C);
+        tensor_cores<<<blocks,threads>>>(A_num_fil, A_num_col, d_A, B_num_fil, B_num_col, d_B, d_C);
     cudaDeviceSynchronize();
 
     cudaEventRecord(start);
     for (int i = 0; i < 20; i++)
-        tiled_matmul_2d<<<blocks,threads>>>(A_num_fil, A_num_col, d_A, B_num_fil, B_num_col, d_B, d_C);
+        tensor_cores<<<blocks,threads>>>(A_num_fil, A_num_col, d_A, B_num_fil, B_num_col, d_B, d_C);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
@@ -111,15 +154,24 @@ int main(){
     printf("GPU: %.3f ms  (%.1f GFLOP/s)\n", gpu_ms, gflops);
     printf("Speedup: %.1fx\n", cpu_ms / gpu_ms);
 
-    
-    cudaMemcpy(h_C, d_C, bytes_C, cudaMemcpyDeviceToHost);
+
+    cudaMemcpy(h_C_2, d_C, bytes_C, cudaMemcpyDeviceToHost);
+
+    //New bench, to help me not miss anything
+    double max_diff = 0.0;
+    int bad_index = -1;
+    for (int i = 0; i < N_C; i++) {
+        double d = fabs((double)h_C[i] - (double)h_C_2[i]);
+        if (d > max_diff) { max_diff = d; bad_index = i; }
+    }
+    printf("Max abs diff: %g  (at index %d)\n", max_diff, bad_index);
 
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
 
     for(int i = 0; i < 5; i ++){
-        printf("%f\n",h_C[i]);
+        printf("%f\n",h_C_2[N_C-i]);
     }
 
     return 0;
